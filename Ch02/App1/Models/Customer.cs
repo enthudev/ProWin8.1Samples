50535e0d6e1f1aa21be076ce2820d85e77a4f326
enthudev <enthudev@live.in>
2014-07-22 08:44:20 +0530

    Initial check in

---
+++ Ch02/App1/Models/Customer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1.Models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value != _firstName)
+                {
+                    _firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value != _lastName)
+                {
+                    _lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        internal void OnPropertyChanged([CallerMemberName] string member = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(member));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Ch02/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using App1.Models;
+using App1.Common;
+namespace App1.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private RelayCommand _UpdateRelayCommand;
+        public Customer MyCustomer { get; set; }
+
+        public MainPageViewModel()
+        {
+            MyCustomer = new Customer()
+            {
+                FirstName = "Bob",
+                LastName = "Smith"
+            };
+        }
+    }
+}
