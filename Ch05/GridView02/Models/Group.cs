637f464b2647b45eb5ad13e2e7946fd9c940fe61
enthudev <enthudev@live.in>
2014-07-23 09:35:16 +0530

    Chapter 5 GridView2

---
+++ Ch05/GridView02/Models/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GridView02.Models
+{
+    public class Group<TKey, TItem>
+    {
+        public TKey Key { get; set; }
+
+        public IList<TItem> Items { get; set; }
+    }
+}
---
+++ Ch05/GridView02/Models/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GridView02.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        private static readonly string[] firstNames =
+        {
+            "Adam", "Bob", "Carl", "David", "Edgar", "Frank", "George",
+            "Harry", "Isaac", "Jesse", "Ken", "Larry"
+        };
+
+        private static readonly string[] lastNames =
+        {
+            "Aaroson", "Bobson", "Carlson", "Davidson", "Enstwhile", "Ferguson", "Harrison", "Isaacson", "Jackson",
+            "Kennelworth", "Levine"
+        };
+
+        private static readonly string[] cities =
+        {
+            "Boston", "New York", "LA", "San Francisco", "Phoenix", "San Jose",
+            "Cincinnati", "Bellevue"
+        };
+
+        public static IEnumerable<Person> CreatePeople(int count)
+        {
+            var people = new List<Person>();
+            var r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var p = new Person()
+                {
+                    FirstName = firstNames[r.Next(firstNames.Length)],
+                    LastName = lastNames[r.Next(lastNames.Length)],
+                    City = cities[r.Next(cities.Length)]
+                };
+                people.Add(p);
+            }
+            return people;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} ({2})", FirstName, LastName, City);
+        }
+    }
+}
